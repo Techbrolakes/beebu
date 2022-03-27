d56944a7a4a7ce45b83dc1904f37916fa5a915ca
Lekan Dar <Lekandar11@gmail.com>
2022-03-26 18:34:11 -1200

    version-1

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#161616",
+        white: "#ffffff",
+        beebu: "#FFE61B",
+      },
+    },
+  },
+  plugins: [],
+};
